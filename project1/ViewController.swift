67379e39083e574d7b43091c3fd6a623568b407e
User <user@Users-MacBook-Pro.local>
2019-09-26 16:51:32 +0530

    first

---
+++ project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by User on 9/22/19.
+//  Copyright © 2019 Arushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
